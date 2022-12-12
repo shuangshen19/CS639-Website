0a586b0599ab91107a2553a0197c129199734061
Sunny Shen <shuangshen19@gmail.com>
2022-12-12 14:48:43 -0600

    Adjust homepage
    Signed-off-by: Sunny Shen <shuangshen19@gmail.com>

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,28 +5,28 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Motivation',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    imgSrc: require('@site/static/img/kinD_paper_result.png').default,
     description: (
       <>
-        This is our motivation.
+        Low-light image enhancement has wild applications in our daily life and in different scientific research fields such as night surveillance, automated driving, etc. This technique can improve the usefulness of an image to satisfy human viewing and with more application applied machine/deep learning methods for improvement, we’d like to research how to use deep learning to enhance low-light images.
       </>
     ),
   },
   {
     title: 'Method',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    imgSrc: require('@site/static/img/decom_layer.jpg').default,
     description: (
       <>
-        This is our method.
+        We reconstruct three neural network(decomposition layer, illumination adjustment layer, and restoration reflectance layer) based on the KinD algorithm using PyTorch. Why we use PyTorch to re-implement? PyTorch is the most popular framework to design deep learning algorithms in recent years. The source code provided by the authors was implemented in an older version of TensorFlow which is hard to make modifications.
       </>
     ),
   },
   {
     title: 'Results Preview',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    imgSrc: require('@site/static/img/final_14_ratio5.jpg').default,
     description: (
       <>
-        This is our results.
+        Our goal is to achieve the result presented in Kindling the Darkness: A Practical Low-light Image Enhancer from Zhang et al. For example, given the input(left image), we could generate the corresponding result shown as the right image.
       </>
     ),
   },
@@ -36,7 +36,7 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={imgSrc} className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
